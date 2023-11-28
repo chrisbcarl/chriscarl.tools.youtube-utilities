cbe389be73cb14310fe4fe158e1540ed53ba0573
Chris Carl <chris.carl@intel.com>
2023-11-27 18:12:32 -0800

    ID - ID

--- apps/copyright-to-timestamps.py
+++ apps/copyright-to-timestamps.py
@@ -70,7 +70,7 @@ if __name__ == '__main__':
                 song = line
                 hidden = 'hidden' in lowline  # Metadata is hidden, as the content in question has not been made publicly available by its provider.
                 if hidden:
-                    song, artist = 'hidden', 'hidden'
+                    song, artist = 'ID', 'ID'
             elif artist is None:
                 if "Video uses this song's melody" in line:
                     if video_uses_this_songs_melody is None:
